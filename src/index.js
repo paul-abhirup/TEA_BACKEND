927d99831accdc9660842d5a13806ea0f6afb520
paul-abhirup <abhiruppaul1249@gmail.com>
2024-03-17 01:58:58 +0530

    1st approach of connecting database

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+// Approach - 1
+// function connectDB(){}
+// connectDB()
+
+// Approach - 2
+// using iife in js
+// its a GOOD PRACTICE to start iife with ;
+// ;()()
+
+
+/*
+  // Noob Approach
+  // this is a bad approach because the index.js got polluted 
+
+
+;( async () => {
+  try {
+   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+   app.on("error", (error) => {
+    console.log("ERRR: ", error);
+    throw error
+   })
+   app.listen(process.env.PORT, ()=>{
+    console.log(`APP is listening to PORT ${process.env.PORT}`)
+   })
+
+  }catch(error){
+    console.error("ERROR: ",error)
+    throw err
+
+  }
+})()
+
+*/
+
+
+
+
+
+
